0bf56c01f2228e692073f4f9894b5d936b33b181
Fernando Vaz <fertwbr@gmail.com>
2025-12-21 16:59:38 -0300

    feat: Add /site route and import SiteProjectPage
    Introduces a new route '/site' to the router, rendering the SiteProjectPage component. Also includes minor formatting changes and adds an explicit React import.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
+import React from 'react';
+import {BrowserRouter, Routes, Route, useLocation} from 'react-router-dom';
+import {AnimatePresence} from 'framer-motion';
 
-import { LanguageProvider } from './context/LanguageContext';
-import { SmoothScrollProvider } from './context/SmoothScrollContext';
+import {LanguageProvider} from './context/LanguageContext';
+import {SmoothScrollProvider} from './context/SmoothScrollContext';
 
 import ScrollToTop from './components/common/ScrollToTop';
 import ErrorBoundary from './components/common/ErrorBoundary';
@@ -13,43 +14,46 @@ import NotFound from './pages/NotFound';
 import PixelCompassPage from './pages/pixel-compass/PixelCompassPage';
 import PixelPulsePage from './pages/pixel-pulse/PixelPulsePage';
 import RedirectToStore from './pages/RedirectToStore';
+import SiteProjectPage from './pages/SiteProjectPage';
 
 function AnimatedRoutes() {
-  const location = useLocation();
+    const location = useLocation();
 
-  return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<PortfolioHome />} />
+    return (
+        <AnimatePresence mode="wait">
+            <Routes location={location} key={location.pathname}>
+                <Route path="/" element={<PortfolioHome/>}/>
 
-        <Route path="/pixelpulse" element={<PixelPulsePage />} />
-        <Route path="/PixelPulse" element={<PixelPulsePage />} />
-        <Route path="/pixelpulse/open" element={<RedirectToStore type="open" appKey="pixelpulse" />} />
-        <Route path="/pixelpulse/open/buy" element={<RedirectToStore type="buy" appKey="pixelpulse" />} />
+                <Route path="/site" element={<SiteProjectPage/>}/>
 
-        <Route path="/pixelcompass" element={<PixelCompassPage />} />
-        <Route path="/PixelCompass" element={<PixelCompassPage />} />
-        <Route path="/pixelcompass/open" element={<RedirectToStore type="open" appKey="pixelcompass" />} />
-        <Route path="/pixelcompass/open/buy" element={<RedirectToStore type="buy" appKey="pixelcompass" />} />
+                <Route path="/pixelpulse" element={<PixelPulsePage/>}/>
+                <Route path="/PixelPulse" element={<PixelPulsePage/>}/>
+                <Route path="/pixelpulse/open" element={<RedirectToStore type="open" appKey="pixelpulse"/>}/>
+                <Route path="/pixelpulse/open/buy" element={<RedirectToStore type="buy" appKey="pixelpulse"/>}/>
 
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </AnimatePresence>
-  );
+                <Route path="/pixelcompass" element={<PixelCompassPage/>}/>
+                <Route path="/PixelCompass" element={<PixelCompassPage/>}/>
+                <Route path="/pixelcompass/open" element={<RedirectToStore type="open" appKey="pixelcompass"/>}/>
+                <Route path="/pixelcompass/open/buy" element={<RedirectToStore type="buy" appKey="pixelcompass"/>}/>
+
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
+        </AnimatePresence>
+    );
 }
 
 export default function App() {
-  return (
-    <ErrorBoundary>
-      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-        <LanguageProvider>
-          <SmoothScrollProvider>
-            <ScrollToTop />
-            <OfflineNotice />
-            <AnimatedRoutes />
-          </SmoothScrollProvider>
-        </LanguageProvider>
-      </BrowserRouter>
-    </ErrorBoundary>
-  );
+    return (
+        <ErrorBoundary>
+            <BrowserRouter future={{v7_startTransition: true, v7_relativeSplatPath: true}}>
+                <LanguageProvider>
+                    <SmoothScrollProvider>
+                        <ScrollToTop/>
+                        <OfflineNotice/>
+                        <AnimatedRoutes/>
+                    </SmoothScrollProvider>
+                </LanguageProvider>
+            </BrowserRouter>
+        </ErrorBoundary>
+    );
 }
